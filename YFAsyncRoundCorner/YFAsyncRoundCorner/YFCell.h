1ea506d41194763f4686575af9073749296f4d73
YFWCQ <1049155621@qq.com>
2017-08-29 10:58:32 +0800

    1

---
+++ YFAsyncRoundCorner/YFAsyncRoundCorner/YFCell.h
@@ -0,0 +1,15 @@
+//
+//  YFCell.h
+//  YFAsyncRoundCorner
+//
+//  Created by FYWCQ on 2017/8/6.
+//  Copyright © 2017年 YFWCQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YFCell : UITableViewCell
+
+@property(nonatomic, strong)UIImageView *headImageView;
+
+@end
